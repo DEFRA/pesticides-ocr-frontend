c9839732ea5f60153ce88ffac0782dd6140d4dd0
MJQ7 <matthew.quinton@defra.gov.uk>
2026-07-28 14:44:50 +0100

    Removed magic numbers
    UPDATED: modified `generateReference` function to use variables for section lengths.
    
    Changes:
    	controller.js updated

--- src/server/routes/qualifying-questions/confirmation/controller.js
+++ src/server/routes/qualifying-questions/confirmation/controller.js
@@ -2,9 +2,11 @@ import { randomInt } from 'node:crypto'
 
 const generateReference = () => {
   const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const firstSectionLength = 3
+  const secondSectionLength = 2
   const digits = (length) => String(randomInt(10 ** length)).padStart(length, '0')
   const letter = LETTERS[randomInt(LETTERS.length)]
-  return `PPP-${digits(3)}-${digits(2)}${letter}`
+  return `PPP-${digits(firstSectionLength)}-${digits(secondSectionLength)}${letter}`
 }
 
 export const get = {
