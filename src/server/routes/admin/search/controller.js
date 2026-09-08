4a408f0f6849c1fca8b385c3ce2bc1e2a17d17d9
MJQ7 <matthew.quinton@defra.gov.uk>
2026-09-08 12:21:20 +0100

    Refined search functionality and improved user feedback for registration results.
    UPDATED: enhanced search result handling to check for registration status
    UPDATED: improved user messaging for no registration found scenario
    
    Changes:
    	src/server/routes/admin/search/controller.js
    	src/server/routes/admin/search/index.njk

--- src/server/routes/admin/search/controller.js
+++ src/server/routes/admin/search/controller.js
@@ -1,9 +1,12 @@
 import { searchRegistration } from '#/server/services/search.js'
 import { buildManualErrorSummary } from '#/client/common/helpers/build-error-summary.js'
+import { statusCodes } from '#/server/common/constants/status-codes.js'
+
+const route = 'admin/search/index'
 
 export const get = {
   handler(_request, h) {
-    return h.view('admin/search/index')
+    return h.view(route)
   }
 }
 
@@ -13,20 +16,21 @@ export const post = {
     const registration = await searchRegistration(reference)
 
     if (!registration) {
-      return h.view('admin/search/index', { reference })
+      return h.view(route, { reference })
     }
 
     if (registration.isBoom) {
       const errorList = buildManualErrorSummary([registration.output.payload]).errorList
-      return h.view('admin/search/index', {
+      return h.view(route, {
         values: request.payload,
         errorList
       })
     }
 
-    return h.view('admin/search/index', {
+    return h.view(route, {
       values: request.payload,
       searched: true,
+      isFound: registration.statusCode !== statusCodes.notFound,
       reference,
       registration
     })
